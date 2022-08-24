beb58ea85a4c91e7f1605748e3e1e284a15618ec
unknown <fxsh312088@gmail.com>
2022-08-24 18:15:47 +0800

    Create Header Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Header from "../components/Header";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css'
+          integrity='sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=='
+          crossOrigin='anonymous'
+          referrerPolicy='no-referrer'
+        />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
